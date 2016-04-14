5d8ffbdd56cd95ad3ecec06164e112943bfba1d6
crsguthrie <cguthrie@bucs.fsw.edu>
2016-04-14 13:40:21 -0400

    Reverted back to the end of Chapter 8 Starting over on Chapter 9.

--- book/Chapter6/app/Amazeriffic/server.js
+++ book/Chapter6/app/Amazeriffic/server.js
@@ -1,57 +1,60 @@
 var express = require("express"),
     http = require("http"),
+    // import the mongoose library
+    mongoose = require("mongoose"),
     app = express(),
-    parser = require("body-parser"),
-    toDos = [
-        { 
-            "description" : "Get groceries",
-            "tags"  : [ "shopping", "chores" ]
-        },
-        { 
-            "description" : "Make up some new ToDos",
-            "tags"  : [ "writing", "work" ]
-        },
-        {
-            "description" : "Prep for Monday's class",
-            "tags"  : [ "work", "teaching" ]
-        },
-        { 
-            "description" : "Answer emails",
-            "tags"  : [ "work" ]
-        },
-        { 
-            "description" : "Take Gracie to the park",
-            "tags"  : [ "chores", "pets" ]
-        },
-        { 
-            "description" : "Finish writing this book",
-            "tags"  : [ "writing", "work" ]
-        }
-    ];
-        
-app.use(express.static(__dirname + "/client"));
+    services,
+    mongoUrl = "mongodb://localhost/amazeriffic";
 
-// tell Express to parse incoming
-// JSON objects
+app.use(express.static(__dirname + "/client"));
+app.use(express.bodyParser());
 app.use(parser.urlencoded({extended:true}));
 
-http.createServer(app).listen(3000);
+if (process.env.VCAP_SERVICES) {
+    services = JSON.parse(process.env.VCAP_SERVICES);
+    mongoUrl = services["mongolab"][0].credentials.uri;
+    console.log(process.env.VCAP_SERVICES);
+}
 
-// This route takes the place of our
-// todos.json file in our example from
-// Chapter 5
+console.log(mongoUrl);
+
+// connect to the amazeriffic data store in mongo
+mongoose.connect(mongoUrl);
+
+// This is our mongoose model for todos
+var ToDoSchema = mongoose.Schema({
+    description: String,
+    tags: [ String ]
+});
+
+var ToDo = mongoose.model("ToDo", ToDoSchema);
+
+http.createServer(app).listen(process.env.PORT || 3000);
+
 app.get("/todos.json", function (req, res) {
-    res.json(toDos);
+    ToDo.find({}, function (err, toDos) {
+	res.json(toDos);
+    });
 });
 
 app.post("/todos", function (req, res) {
-    // the object is now stored in req.body
-    var newToDo = req.body;
-
-    console.log(newToDo);
-
-    toDos.push(newToDo);
-
-    // send back a simple object
-    res.json({"message":"You posted to the server!"});
+    console.log(req.body);
+    var newToDo = new ToDo({"description":req.body.description, "tags":req.body.tags});
+    newToDo.save(function (err, result) {
+	if (err !== null) {
+	    // the element did not get saved!
+	    console.log(err);
+	    res.send("ERROR");
+	} else {
+	    // our client expects *all* of the todo items to be returned, so we'll do
+	    // an additional request to maintain compatibility
+	    ToDo.find({}, function (err, result) {
+		if (err !== null) {
+		    // the element did not get saved!
+		    res.send("ERROR");
+		}
+		res.json(result);
+	    });
+	}
+    });
 });
